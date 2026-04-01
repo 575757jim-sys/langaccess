d751652759d4274efb253b477689f303b761a21a
575757jim-sys <575757jim@gmail.com>
2026-03-31 19:20:56 -0700

    Updated package-lock.json

---
+++ test-generate-card.js
@@ -0,0 +1,14 @@
+import { generateCardWithQR } from './src/utils/generateCardWithQR.ts';
+
+async function test() {
+  try {
+    console.log('Running generateCardWithQR with batchId: "oakland-test-1"...\n');
+    const result = await generateCardWithQR('oakland-test-1');
+    console.log('\nReturned URL:', result);
+  } catch (error) {
+    console.error('Error:', error.message);
+    process.exit(1);
+  }
+}
+
+test();
