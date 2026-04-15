7f893697f0e63a038c1116d7a9d40d6e281090c9
575757jim-sys <575757jim@gmail.com>
2026-04-15 10:43:45 -0700

    Pin CORS origin to langaccess.org

--- supabase/functions/create-cert-checkout/index.ts
+++ supabase/functions/create-cert-checkout/index.ts
@@ -1,9 +1,9 @@
 import "jsr:@supabase/functions-js/edge-runtime.d.ts";
 
 const corsHeaders = {
-  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Origin": "https://langaccess.org",
   "Access-Control-Allow-Methods": "POST, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Client-Info, Apikey",
+  "Access-Control-Allow-Headers": "authorization, x-client-info, apikey, content-type",
 };
 
 Deno.serve(async (req: Request) => {
